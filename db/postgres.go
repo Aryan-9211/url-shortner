554b116ec84f2cc6885c79437ef0483fd75defa6
Aryan Mangla <aryan.mangla@grofers.com>
2024-11-20 02:12:26 +0530

    add postgres

--- db/postgres.go
+++ db/postgres.go
@@ -1,1 +1,23 @@
 package db
+
+import "log"
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func InitPostgres() {
+	db, err := sqlx.Connect("postgres", "user=postgres dbname=yourdatabase sslmode=disable password=yourpassword host=localhost")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Successfully Connected")
+	}
+}
--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module github.com/Aryan-9211/url-shortner
 
 go 1.21.9
 
+require github.com/redis/go-redis/v9 v9.7.0
+
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/redis/go-redis/v9 v9.7.0 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 )
